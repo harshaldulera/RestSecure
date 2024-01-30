d1b60a1dccaf00db1249347216696023dc55724f
Harshal Dulera <harshaldulera82@gmail.com>
2024-01-30 22:04:40 +0530

    Tailwind init

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,8 +4,11 @@ const Home = () => {
     return (
         <>
             <div className="flex">
-                <Sidebar />
-                <main className="flex-1">
+                <div className="bg-gray-800 w-64 flex-none overflow-hidden">
+                    <Sidebar />
+                </div>
+                {/* <Sidebar /> */}
+                <main className="flex-1 p-4">
                     <h1>Welcome to RestSecure!</h1>
                 </main>
             </div>
